f67e69b37d733e074fa0135f3c82aa7ba51ed369
Faizan Mohammed <faizanmohammed416@gmail.com>
2023-11-28 23:40:53 +0530

    Adding elements to GUI.

--- src/GUI.java
+++ src/GUI.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 
 public class GUI {
     JFrame frame;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu fileMenu, editMenu, formatMenu, colorMenu;
+    JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
     public GUI(){
 
     }
