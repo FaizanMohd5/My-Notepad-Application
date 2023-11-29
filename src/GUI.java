21e0ab158c410427a0296bf137ba23ca5fb2eddc
Faizan Mohammed <faizanmohammed416@gmail.com>
2023-11-29 21:40:32 +0530

    Modifying class GUI.

--- src/GUI.java
+++ src/GUI.java
@@ -8,6 +8,27 @@ public class GUI {
     JMenu fileMenu, editMenu, formatMenu, colorMenu;
     JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
     public GUI(){
+        createGUI();
+    }
+    public void createGUI(){
+        frame = new JFrame("Notepad");
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        textArea = new JTextArea();
+        frame.add(textArea);
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        editMenu = new JMenu("Edit");
+        colorMenu = new JMenu("Color");
+        formatMenu = new JMenu("Format");
+
+        frame.add(menuBar);
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(colorMenu);
+        menuBar.add(formatMenu);
     }
 }
